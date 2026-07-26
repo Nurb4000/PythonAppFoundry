7e844ff8cc5fb0ae758319db64d5eb098ec2700c
ziggy <ziggy@localhost.localdomain>
2026-07-26 11:36:41 -0400

    Add admin_query_extended.py blueprint for extended query management

--- app/routes/admin_query_extended.py
+++ app/routes/admin_query_extended.py
@@ -7,7 +7,7 @@ query_extended_bp = Blueprint('query_extended', __name__)
 @query_extended_bp.route('/queries/<int:id>/enable')
 @developer_or_admin_required
 def enable_query(id):
-    """Enable a query report."""
+    """Enable a query report by setting a schedule."""
     from app.models import QueryReport
     
     query = db.session.get(QueryReport, id)
@@ -29,7 +29,7 @@ def enable_query(id):
 @query_extended_bp.route('/queries/<int:id>/disable')
 @developer_or_admin_required
 def disable_query(id):
-    """Disable a query report."""
+    """Disable a query report by clearing the schedule."""
     from app.models import QueryReport
     
     query = db.session.get(QueryReport, id)
@@ -46,7 +46,7 @@ def disable_query(id):
 @query_extended_bp.route('/queries/<int:id>/test')
 @developer_or_admin_required
 def test_query(id):
-    """Test a query report."""
+    """Test a query report by running it."""
     from app.models import QueryReport
     
     query = db.session.get(QueryReport, id)
@@ -63,3 +63,43 @@ def test_query(id):
         flash(f'Query failed: {e}', 'error')
     
     return redirect(url_for('admin.edit_query', id=id))
+
+
+@query_extended_bp.route('/queries/bulk-enable', methods=['POST'])
+@developer_or_admin_required
+@csrf_protect
+def bulk_enable_queries():
+    """Enable multiple queries at once."""
+    from app.models import QueryReport
+    
+    query_ids = request.form.getlist('query_ids')
+    enabled = 0
+    for qid in query_ids:
+        query = db.session.get(QueryReport, int(qid))
+        if query and not query.schedule_cron:
+            query.schedule_cron = '*/5 * * * *'
+            enabled += 1
+    
+    db.session.commit()
+    flash(f'Enabled {enabled} query(s)')
+    return redirect(url_for('admin.list_queries'))
+
+
+@query_extended_bp.route('/queries/bulk-disable', methods=['POST'])
+@developer_or_admin_required
+@csrf_protect
+def bulk_disable_queries():
+    """Disable multiple queries at once."""
+    from app.models import QueryReport
+    
+    query_ids = request.form.getlist('query_ids')
+    disabled = 0
+    for qid in query_ids:
+        query = db.session.get(QueryReport, int(qid))
+        if query and query.schedule_cron:
+            query.schedule_cron = ''
+            disabled += 1
+    
+    db.session.commit()
+    flash(f'Disabled {disabled} query(s)')
+    return redirect(url_for('admin.list_queries'))
